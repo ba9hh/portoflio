c8a609781aad1e37b7c46e63710bc6bec15711a7
ba9hh <zedtourheart@gmail.com>
2026-01-09 22:23:21 +0100

    first commit

--- frontend/src/components/description.jsx
+++ frontend/src/components/description.jsx
@@ -6,15 +6,15 @@ const Description = () => {
   const { t } = useTranslation();
   return (
     <div className="w-full border-white bg-white dark:bg-gray-800 md:pt-10 pb-7 flex justify-center md:px-10">
-      <div className="flex flex-col md:flex-row items-center justify-around gap-5 border rounded-md md:px-5 py-2 border-white md:bg-gray-200 dark:bg-gray-700">
-        <div className="md:hidden md:w-1/2 flex justify-center border">
+      <div className="flex flex-col md:flex-row items-center justify-around md:gap-5 border rounded-md md:px-5 py-2 border-white md:bg-gray-200 dark:bg-gray-700">
+        <div className="md:hidden md:w-1/2 flex justify-center">
           <img
             src={me}
             className="w-1/2 rounded-full aspect-square object-cover p-10"
           />
         </div>
-        <div className="w-full md:w-1/2 flex flex-col items-center justify-center gap-2 border">
-          <h1 className="text-gray-700 dark:text-white text-3xl font-bold">
+        <div className="w-full md:w-1/2 flex flex-col items-center justify-center gap-2">
+          <h1 className="text-gray-700 dark:text-white text-3xl font-bold hidden md:block">
             {t("name")}
           </h1>
           <p className="text-gray-500 dark:text-gray-200 font-semibolt text-center text-lg">
--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -6,6 +6,7 @@ import language from "../assets/language.png";
 import icon from "../assets/icon.png";
 import { useTheme } from "../ThemeContext";
 import { useTranslation } from "react-i18next";
+import menu from "../assets/menu.svg";
 
 const NavBar = () => {
   const { isDarkMode, setIsDarkMode } = useTheme();
@@ -24,55 +25,70 @@ const NavBar = () => {
     document.dir = savedLang === "ar" ? "rtl" : "ltr";
   }, [i18n.language]);
   return (
-    <div className="w-full flex justify-between dark:border">
-      <div className="w-fit flex gap-4 px-2 py-1.5 border dark:border-white border-black rounded-md">
-        <img src={icon} className="w-7" />
-      </div>
-      <div className="w-fit flex gap-4 px-4 py-1.5 border dark:border-white border-gray-800 rounded-md">
-        <h1 className="text-gray-800 dark:text-gray-100 underline">
-          {t("about")}
-        </h1>
-        <h1 className="text-gray-800 dark:text-gray-100">{t("projects")}</h1>
-        <h1 className="text-gray-800 dark:text-gray-100">{t("tools")}</h1>
-      </div>
-      <div className="relative w-fit flex gap-4 px-4 py-1.5 border border-black dark:border-white rounded-md">
-        <img
-          src={isDarkMode ? nightMode2 : nightMode}
-          className="w-6 h-6"
-          onClick={() => setIsDarkMode((prev) => !prev)}
-        />
-
-        <div className="">
+    <>
+      <div className="hidden w-full md:flex justify-between dark:border">
+        <div className="w-fit flex gap-4 px-2 py-1.5 border dark:border-white border-black rounded-md">
+          <img src={icon} className="w-7" />
+        </div>
+        <div className="w-fit flex gap-4 px-4 py-1.5 border dark:border-white border-gray-800 rounded-md">
+          <h1 className="text-gray-800 dark:text-gray-100 underline">
+            {t("about")}
+          </h1>
+          <h1 className="text-gray-800 dark:text-gray-100">{t("projects")}</h1>
+          <h1 className="text-gray-800 dark:text-gray-100">{t("tools")}</h1>
+        </div>
+        <div className="relative w-fit flex gap-4 px-4 py-1.5 border border-black dark:border-white rounded-md">
           <img
-            src={isDarkMode ? language2 : language}
+            src={isDarkMode ? nightMode2 : nightMode}
             className="w-6 h-6"
-            onClick={() => setLangMenuOpen(!langMenuOpen)}
+            onClick={() => setIsDarkMode((prev) => !prev)}
           />
-          {langMenuOpen && (
-            <div className="absolute mt-4 bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-white rounded shadow-lg w-40 text-center z-10 -right-0.5">
-              <button
-                onClick={() => changeLanguage("en")}
-                className="block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white"
-              >
-                English
-              </button>
-              <button
-                onClick={() => changeLanguage("fr")}
-                className="block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white"
-              >
-                Français
-              </button>
-              <button
-                onClick={() => changeLanguage("ar")}
-                className="block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white"
-              >
-                العربية
-              </button>
-            </div>
-          )}
+
+          <div className="">
+            <img
+              src={isDarkMode ? language2 : language}
+              className="w-6 h-6"
+              onClick={() => setLangMenuOpen(!langMenuOpen)}
+            />
+            {langMenuOpen && (
+              <div className="absolute mt-4 bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-white rounded shadow-lg w-40 text-center z-10 -right-0.5">
+                <button
+                  onClick={() => changeLanguage("en")}
+                  className="block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white"
+                >
+                  English
+                </button>
+                <button
+                  onClick={() => changeLanguage("fr")}
+                  className="block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white"
+                >
+                  Français
+                </button>
+                <button
+                  onClick={() => changeLanguage("ar")}
+                  className="block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white"
+                >
+                  العربية
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="md:hidden flex justify-between items-center p-4 border-b">
+        <div className="relative rounded-xl border border-stone-300 bg-white/80 p-2 dark:border-stone-700 dark:bg-stone-800">
+          <img src={menu} className="w-5 h-5" />
+        </div>
+        <h1 className="text-sky-600 font-semibold text-lg">Ezzedine Jlidi</h1>
+        <div className="relative rounded-xl border border-stone-300 bg-white/80 p-2 dark:border-stone-700 dark:bg-stone-800">
+          <img
+            src={isDarkMode ? nightMode2 : nightMode}
+            className="w-5 h-5"
+            onClick={() => setIsDarkMode((prev) => !prev)}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
